3bf152559a094f0363c7eba0d60b193c5ba6e628
Amirjani <amir.jani500@gmail.com>
2021-06-08 10:56:46 +0430

    feature: linear -> tuple

--- linear/tuple.go
+++ linear/tuple.go
@@ -1,3 +1,19 @@
 // A tuple is a finite sorted *list* of elements
 // It is a data structure that groups data
 package main
+
+import "fmt"
+
+// * this return a tuple
+func powerSeries(a int) (int, int) {
+	return a * a, a * a * a
+}
+
+func main() {
+	var square int
+	var cube int
+
+	square, cube = powerSeries(3)
+
+	fmt.Println("Square: ", square, "Cube: ", cube)
+}
