fc67141a2776c26f085a1f70e76f9b21f057919a
Amirjani <amir.jani500@gmail.com>
2021-06-12 11:41:41 +0430

    feature: complexity -> logarithmic

---
+++ complexity/logarithmic.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+type Tree struct {
+	LeftNode  *Tree
+	Value     int
+	RightNode *Tree
+}
+
+func (tree *Tree) insert(m int) {
+	if tree != nil {
+		if tree.LeftNode == nil {
+			tree.LeftNode = &Tree{nil, m, nil}
+		} else {
+			if tree.RightNode == nil {
+				tree.RightNode = &Tree{nil, m, nil}
+			} else {
+				if tree.LeftNode != nil {
+					tree.LeftNode.insert(m)
+				} else {
+					tree.RightNode.insert(m)
+				}
+			}
+		}
+	} else {
+		tree = &Tree{nil, m, nil}
+	}
+}
+
+func print(tree *Tree) {
+	if tree != nil {
+		fmt.Println(" Value", tree.Value)
+		fmt.Printf("Tree Node Left")
+		print(tree.LeftNode)
+		fmt.Printf("Tree Node Right")
+		print(tree.RightNode)
+	} else {
+		fmt.Printf("Nil\n")
+	}
+}
+
+func main() {
+	var tree *Tree = &Tree{nil, 1, nil}
+	print(tree)
+	tree.insert(3)
+	print(tree)
+	tree.insert(5)
+	print(tree)
+	tree.LeftNode.insert(7)
+	print(tree)
+}
